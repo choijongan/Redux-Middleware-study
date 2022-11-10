1216cf0160bfd42a98e456cbbb706cd67e16b199
choijongan <mmm7134@naver.com>
2022-11-10 19:00:00 +0900

    미들웨어 학습을 위한 쇼핑몰 복습

---
+++ hnm-react-router-practice/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetail from './page/ProductDetail';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<ProductAll/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/product/:id' element={<ProductDetail/>}/>
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
