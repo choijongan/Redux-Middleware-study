0cae6ff1d039b6f1b718b36fb8e64469cee85628
choijongan <mmm7134@naver.com>
2022-11-11 19:53:55 +0900

    useNavigate로 메인으로 돌아가기

--- hnm-react-router-practice/src/App.js
+++ hnm-react-router-practice/src/App.js
@@ -5,16 +5,19 @@ import ProductAll from './page/ProductAll';
 import Login from './page/Login';
 import ProductDetail from './page/ProductDetail';
 import Navbar from './component/Navbar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const[authenticate, setAuthenticate] = useState(false) //true면 로그인, false면 로그인 안됨.
+  useEffect(() => {
+    console.log('aaa', authenticate)
+  }, [authenticate])
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path='/' element={<ProductAll/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/login' element={<Login setAuthenticate={setAuthenticate}/>}/>
         <Route path='/product/:id' element={<ProductDetail/>}/>
       </Routes>
       
