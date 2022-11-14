3098129d5e8c00382eeea6b5d356a9ce67a413d7
choijongan <mmm7134@naver.com>
2022-11-14 19:56:08 +0900

    상품 디테일 페이지

--- hnm-react-router-practice/src/page/ProductDetail.js
+++ hnm-react-router-practice/src/page/ProductDetail.js
@@ -1,21 +1,61 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col, Button, Dropdown, Alert } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 
 const ProductDetail = () => {
   let{id} = useParams()
+  const [product, setProduct] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const getProductDetail= async()=>{
+    setLoading(true);
     let url = `http://localhost:5000/products/${id}`
     let response = await fetch(url)
     let data = await response.json()
-    console.log(data)
+    setLoading(false);
+    
+    setProduct(data)
   }
   useEffect(()=>{
     getProductDetail()
   },[])
-  
+  if (loading || product == null) return <h1>Loading</h1>;
   return (
-    <div>
-    </div>
+    <Container className='product-detail-card'>
+      {error ? (
+        <Alert variant="danger" className="text-center">
+          {error}
+        </Alert>
+      ) : (
+      <Row>
+        <Col className='product-detail-img'>
+         <img src={product?.img} />
+        </Col>
+        <Col>
+         <div className="product-info">{product.title}</div>
+         <div className="product-info">₩ {product.price}</div>
+         <div className="choice">
+              {product.choice ? "Conscious choice" : ''}
+          </div>
+          <Dropdown className="drop-down">
+              <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+                사이즈 선택
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                {product?.size.length > 0 &&
+                  product.size.map((item) => (
+                    <Dropdown.Item href="#/action-1">{item}</Dropdown.Item>
+                  ))}
+              </Dropdown.Menu>
+            </Dropdown>
+            <Button variant="dark" className="add-button">
+              추가
+            </Button>
+        </Col>
+      </Row>
+      )}
+    </Container>
   )
 }
 
