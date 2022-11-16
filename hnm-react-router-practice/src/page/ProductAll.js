4ce38948c066ab59675025381a5fed61fab93623
choijongan <mmm7134@naver.com>
2022-11-16 20:26:36 +0900

    search기능 완성

--- hnm-react-router-practice/src/page/ProductAll.js
+++ hnm-react-router-practice/src/page/ProductAll.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { useSearchParams } from 'react-router-dom'
 import ProductCard from '../component/ProductCard'
 
 const ProductAll = () => {
     const [ProductList,setProductList]=useState([])
-    const getProducts= async ()=>{
-      let url = 'http://localhost:5000/products'
+    const [query, setQuery] = useSearchParams()
+    const getProducts= async () => {
+      let searchQuery = query.get('q') || ''
+      let url = `http://localhost:5000/products?q=${searchQuery}` //json-server제공 기능. q 서치
       let response = await fetch(url)
       let data = await response.json()
       setProductList(data)
     }
     useEffect(()=>{
       getProducts()
-    },[])
+    }, [query]) //어레이 안에다가 query를 넣어주면 query가 바뀔때마다 다시 호출해줌.
   return (
     <div>
           <Container>
