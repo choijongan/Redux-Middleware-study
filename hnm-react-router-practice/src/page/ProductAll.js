8683421e3e1bba60ba7be14c629151f286d504ca
choijongan <mmm7134@naver.com>
2022-11-22 19:13:42 +0900

    combineReducer 적용 성공!

--- hnm-react-router-practice/src/page/ProductAll.js
+++ hnm-react-router-practice/src/page/ProductAll.js
@@ -7,7 +7,8 @@ import { useDispatch, useSelector } from 'react-redux'
 
 const ProductAll = () => {
     // const [ProductList,setProductList]=useState([])
-    const ProductList = useSelector((state) => state.ProductList)
+    // const ProductList = useSelector((state) => state.ProductList) 
+    const ProductList = useSelector((state) => state.product.ProductList)//어떤 reducer의 state를 읽어올지 얘기해야됨. combineReducer를 썻다면 언급을 해줘야됨.
     const [query, setQuery] = useSearchParams()
     const dispatch = useDispatch()
     const getProducts =  () => {
