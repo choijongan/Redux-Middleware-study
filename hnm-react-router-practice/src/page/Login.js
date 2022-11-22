50addae40eb6b43fbe7d8161e746f55793266bcc
choijongan <mmm7134@naver.com>
2022-11-22 18:23:36 +0900

    id, password 읽어오기 완료

--- hnm-react-router-practice/src/page/Login.js
+++ hnm-react-router-practice/src/page/Login.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { authenticateAction } from '../redux/actions/authenticateAction';
 
 const Login = ({setAuthenticate}) => {
+  const [id,setId]=useState('')
+  const [password,setPassword]=useState('')
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   const loginUser = (event) => {
     event.preventDefault()
-    setAuthenticate(true)
+    // setAuthenticate(true)
+    dispatch(authenticateAction.login(id,password))
     navigate('/')
   }
  return (
@@ -16,7 +22,7 @@ const Login = ({setAuthenticate}) => {
     <Form onSubmit={(event)=>loginUser(event)}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>이메일</Form.Label>
-        <Form.Control type="email" placeholder="이메일을 입력해주세요." />
+        <Form.Control type="email" placeholder="이메일을 입력해주세요." onChange={(event)=>setId(event.target.valueAsDate)} />
         <Form.Text className="text-muted">
           귀하의 이메일을 다른 사람과 공유하지 않습니다.
         </Form.Text>
@@ -24,7 +30,7 @@ const Login = ({setAuthenticate}) => {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>비밀번호</Form.Label>
-        <Form.Control type="password" placeholder="비밀번호를 입력해주세요." />
+        <Form.Control type="password" placeholder="비밀번호를 입력해주세요." onChange={(event)=>setPassword(event.target.value)}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="자동로그인 방지" />
