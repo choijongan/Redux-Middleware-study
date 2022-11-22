0b41546d70e20cbfb9bd62a505097b53c2ec4bb4
choijongan <mmm7134@naver.com>
2022-11-22 18:55:47 +0900

    새로운 reducer 생성 완료

--- hnm-react-router-practice/src/redux/actions/authenticateAction.js
+++ hnm-react-router-practice/src/redux/actions/authenticateAction.js
@@ -1,5 +1,7 @@
-function login (){
-    return (dispatch, getState) => {}
+function login (id,password){
+    return (dispatch, getState) => {
+        dispatch({ type:'LOGIN_SUCCESS',payload: { id, password } })
+    }
 }
 
 export const authenticateAction = { login }
