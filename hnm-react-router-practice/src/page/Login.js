960b1934a2f051f4ef89fb4be49cc7bb5a4862e6
choijongan <mmm7134@naver.com>
2022-11-23 22:07:31 +0900

    데브툴 적용 성공!

--- hnm-react-router-practice/src/page/Login.js
+++ hnm-react-router-practice/src/page/Login.js
@@ -22,7 +22,7 @@ const Login = ({setAuthenticate}) => {
     <Form onSubmit={(event)=>loginUser(event)}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>이메일</Form.Label>
-        <Form.Control type="email" placeholder="이메일을 입력해주세요." onChange={(event)=>setId(event.target.valueAsDate)} />
+        <Form.Control type="email" placeholder="이메일을 입력해주세요." onChange={(event)=>setId(event.target.value)} />
         <Form.Text className="text-muted">
           귀하의 이메일을 다른 사람과 공유하지 않습니다.
         </Form.Text>
--- hnm-react-router-practice/src/redux/store.js
+++ hnm-react-router-practice/src/redux/store.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 // import productReducer from "./reducers/productReducer"
 
-let store = createStore(rootReducer, applyMiddleware(thunk))
+let store = createStore(
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(thunk)))
+
 // let store = createStore(productReducer, applyMiddleware(thunk))
 
 export default store;
